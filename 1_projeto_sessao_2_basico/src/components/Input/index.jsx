74509cbb7e596514ec2f94382ebf70ca25c61b19
root <root@luisitcho.>
2025-01-22 00:14:35 -0300

    curso react - init

--- 1_projeto_sessao_2_basico/src/components/Input/index.jsx
+++
@@ -1,7 +0,0 @@
-import './styles.css';
-
-export const Input = ({ onChange, value }) => {
-    return (
-        <input type="search" name="" id="" className='input-search' onChange={onChange} value={value} />
-    )
-}
