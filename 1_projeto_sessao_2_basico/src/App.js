f85fd0c1c510ed446ff609d790e7d2ae1f8c81bb
luisitcho <luishenrisc1@gmail.com>
2025-02-15 04:23:10 -0300

    curso react - componentes de classe com estado (stateful)

--- 1_projeto_sessao_2_basico/src/App.js
+++ 1_projeto_sessao_2_basico/src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+	state = {
+		name: 'Luis Henrique',
+		counter: 0
+	}
+
+	handlePClicked = () => {
+		this.setState({ name: 'Peppa' });
+	}
+
+	handleAClicked = (event) => {
+		event.preventDefault();
+		const { counter } = this.state;
+		this.setState({ counter: counter + 1 });
+	}
+	render() {
+		const { name, counter } = this.state;
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p onClick={this.handlePClicked}>
+						{name}
+					</p>
+					<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" onClick={this.handleAClicked}>
+						Learn React
+					</a>
+					<p className="counter">{counter}</p>
+				</header>
+			</div>
+		);
+	}
 }
-
-export default App;
+// export default App;
