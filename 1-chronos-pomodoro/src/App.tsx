719cef6d70d4b2355b1cc89322dd7207b40266dc
luisitcho <luishenrisc1@gmail.com>
2025-03-30 20:23:36 -0300

    curso react - Propriedades (props) e children de componentes no React

--- 1-chronos-pomodoro/src/App.tsx
+++ 1-chronos-pomodoro/src/App.tsx
@@ -6,7 +6,11 @@ import './scss/style.scss';
 export function App() {
     return (
         <>
-            <Heading />
+            <Heading attr={124} teste={'String'}>
+                Olá Mundo 1!
+            </Heading>
+            <Heading>Olá Mundo 2!</Heading>
+            <Heading>Olá Mundo 3!</Heading>
         </>
     );
 }
--- 1-chronos-pomodoro/src/components/Heading.tsx
+++ 1-chronos-pomodoro/src/components/Heading.tsx
@@ -1,5 +1,20 @@
 import styles from './Heading.module.scss';
 
-export function Heading() {
-    return <h1 className={styles.heading}>Olá Mundo!</h1>;
+interface HeadingProps {
+    teste?: string;
+    attr?: number;
+    children: React.ReactNode;
+}
+
+export function Heading(props: HeadingProps) {
+    console.log(props);
+    return (
+        <h1
+            className={styles.heading}
+            aria-label={props.teste}
+            aria-hidden={props.attr ? 'true' : 'false'}
+        >
+            {props.children}
+        </h1>
+    );
 }
