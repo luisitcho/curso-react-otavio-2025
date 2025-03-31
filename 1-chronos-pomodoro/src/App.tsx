435df445319af3e88f729abb0ee650b0b478e51c
luisitcho <luishenrisc1@gmail.com>
2025-03-30 23:11:52 -0300

    curso react - Criando a estrutura HTML e CSS para cada seção

--- 1-chronos-pomodoro/src/App.tsx
+++ 1-chronos-pomodoro/src/App.tsx
@@ -3,13 +3,18 @@ import { TimerIcon } from 'lucide-react';
 
 export function App() {
     return (
-        <div className='container mx-auto'>
-            <Heading>
-                Olá Mundo 1!{' '}
-                <button>
-                    <TimerIcon />
-                </button>
-            </Heading>
-        </div>
+        <>
+            <div className='container'>
+                <div className='content'>
+                    <section>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Consequuntur molestiae vero distinctio, minus iste quia
+                        reprehenderit rem possimus nobis dolore illo asperiores
+                        vitae id veritatis ullam laboriosam nemo nesciunt
+                        magnam?
+                    </section>
+                </div>
+            </div>
+        </>
     );
 }
