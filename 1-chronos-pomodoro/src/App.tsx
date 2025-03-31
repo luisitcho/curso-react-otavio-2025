909b417c376ed38727f09ab53094395f73c26a74
luisitcho <luishenrisc1@gmail.com>
2025-03-31 00:58:36 -0300

    curso react - Movendo o Container para seu próprio React Component

--- 1-chronos-pomodoro/src/App.tsx
+++ 1-chronos-pomodoro/src/App.tsx
@@ -1,20 +1,15 @@
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container/Container';
 
 export function App() {
     return (
         <>
-            <div className='container'>
-                <div className='content'>
-                    <section>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Consequuntur molestiae vero distinctio, minus iste quia
-                        reprehenderit rem possimus nobis dolore illo asperiores
-                        vitae id veritatis ullam laboriosam nemo nesciunt
-                        magnam?
-                    </section>
-                </div>
-            </div>
+            <Container>
+                <section>LOGO</section>
+            </Container>
+
+            <Container>
+                <section>Menu</section>
+            </Container>
         </>
     );
 }
