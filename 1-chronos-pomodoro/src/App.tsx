5ae6314ed7e292835b0796e96387eea773cb95f3
luisitcho <luishenrisc1@gmail.com>
2025-04-01 21:09:28 -0300

    curso react - Input: um componente um pouco mais complexo

--- 1-chronos-pomodoro/src/App.tsx
+++ 1-chronos-pomodoro/src/App.tsx
@@ -1,6 +1,6 @@
 import { Container } from './components/Container';
 import { ContDown } from './components/ContDown';
-import { Form } from './components/Form';
+import { DefaultInput } from './components/DefaultInput';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 
@@ -20,7 +20,24 @@ export function App() {
             </Container>
 
             <Container>
-                <Form />
+                <form className='form flex flex-col justify-center gap-4'>
+                    <fieldset className='fieldset flex flex-col justify-center items-center gap-4'>
+                        <DefaultInput id='texto' type='text' />
+                    </fieldset>
+
+                    <fieldset className='fieldset flex flex-col justify-center items-center gap-4'>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </fieldset>
+
+                    <fieldset className='fieldset flex flex-col justify-center items-center gap-4'>
+                        <p>Ciclos</p>
+                        <p>0 0 0 0 0</p>
+                    </fieldset>
+
+                    <fieldset className='fieldset flex flex-col justify-center items-center gap-4'>
+                        <button type='submit'>Enviar</button>
+                    </fieldset>
+                </form>
             </Container>
         </>
     );
