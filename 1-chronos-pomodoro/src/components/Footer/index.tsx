4157a3cfb246a164e6f0d2bdabceea7ccc8654a0
luisitcho <luishenrisc1@gmail.com>
2025-04-04 01:58:04 -0300

    curso react - useEffect | hook feito para trabalhar com efeitos colaterais no componente (Dark Theme)

--- 1-chronos-pomodoro/src/components/Footer/index.tsx
+++ 1-chronos-pomodoro/src/components/Footer/index.tsx
@@ -5,10 +5,16 @@ export function Footer() {
         <footer
             className={`${styles.footer} flex flex-col items-center justify-center gap-4`}
         >
-            <a href='#' className={`${styles.footer__link} text-[13px] font-medium`}>
+            <a
+                href='#'
+                className={`${styles.footer__link} dark:text-[#aab3cc] text-[13px] font-medium`}
+            >
                 Entenda como funciona a técnica pomodoro
             </a>
-            <a href='#' className={`${styles.footer__link} text-[18px] font-medium`}>
+            <a
+                href='#'
+                className={`${styles.footer__link} dark:text-[#aab3cc] text-[18px] font-medium`}
+            >
                 Chronos Pomodoro &copy; {new Date().getFullYear()}
             </a>
         </footer>
--- 1-chronos-pomodoro/src/components/Menu/index.tsx
+++ 1-chronos-pomodoro/src/components/Menu/index.tsx
@@ -1,21 +1,60 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.scss';
+import { useState, useEffect } from 'react';
+
+type AvailableThemes = 'light' | 'dark';
 
 export function Menu() {
+    const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+    function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement>) {
+        event?.preventDefault();
+        setTheme(prevTheme => {
+            const newTheme = prevTheme === 'dark' ? 'light' : 'dark';
+            return newTheme;
+        });
+    }
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme);
+
+        document.documentElement.classList.remove('light', 'dark');
+        document.documentElement.classList.add(theme);
+    }, [theme]);
+
     return (
-        <nav
-            className={`${styles.menu} m-auto flex items-center gap-4 justify-center`}
-        >
-            <a href='#' className={styles.menu__item}>
+        <nav className={`${styles.menu} m-auto flex items-center gap-4 justify-center`}>
+            <a
+                href='#'
+                className={styles.menu__item}
+                aria-label='Ir para a página inicial'
+                title='Ir para a página inicial'
+            >
                 <HouseIcon />
             </a>
-            <a href='#' className={styles.menu__item}>
+            <a
+                href='#'
+                className={styles.menu__item}
+                aria-label='Ir para o histórico'
+                title='Ir para o histórico'
+            >
                 <HistoryIcon />
             </a>
-            <a href='#' className={styles.menu__item}>
+            <a
+                href='#'
+                className={styles.menu__item}
+                aria-label='Ir para as configurações'
+                title='Ir para as configurações'
+            >
                 <SettingsIcon />
             </a>
-            <a href='#' className={styles.menu__item}>
+            <a
+                href='#'
+                className={styles.menu__item}
+                aria-label='Alterar tema'
+                title='Alterar tema'
+                onClick={handleThemeChange}
+            >
                 <SunIcon />
             </a>
         </nav>
