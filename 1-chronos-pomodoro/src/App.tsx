e1eaaebbd2bbf8093c5ba2e68f1bb32785dde4be
luisitcho <luishenrisc1@gmail.com>
2025-04-06 07:24:51 -0300

    curso react - Estado global da aplicação com useState e comunicação via props

--- 1-chronos-pomodoro/src/App.tsx
+++ 1-chronos-pomodoro/src/App.tsx
@@ -1,8 +1,26 @@
+import { useState } from 'react';
 import { Home } from './pages/Home';
+import { TaskStateModel } from './models/TaskStateModel';
 
 import './scss/base/_reboot.scss';
 import './main.css';
 
+const initialState: TaskStateModel = {
+    tasks: [],
+    secondsRemaining: 0,
+    formattedSecondsRemaining: '00:00',
+    activeTask: null,
+    currentCycle: 0,
+    config: {
+        workTime: 25,
+        shortBreakTime: 5,
+        longBreakTime: 15,
+    },
+};
+
 export function App() {
-    return <Home />;
+    const [state, setState] = useState(initialState);
+
+    console.log('App', state);
+    return <Home state={state} setState={setState} />;
 }
--- 1-chronos-pomodoro/src/pages/Home/index.tsx
+++ 1-chronos-pomodoro/src/pages/Home/index.tsx
@@ -1,12 +1,29 @@
 import { Container } from '../../components/Container';
 import { ContDown } from '../../components/ContDown';
 import { Form } from '../../components/Form';
+import { TaskStateModel } from '../../models/TaskStateModel';
 import { MainTemplate } from '../../templates/MainTemplate';
 
-export function Home() {
+type HomeProps = {
+    state: TaskStateModel;
+    setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+export function Home(props: HomeProps) {
+    const { state, setState } = props;
+
+    function handleClick() {
+        setState(prevState => {
+            return {
+                ...prevState,
+                currentCycle: 5,
+            };
+        });
+    }
     return (
         <MainTemplate>
             <Container>
+                <button onClick={handleClick}>Clicar</button>
                 <ContDown />
             </Container>
 
