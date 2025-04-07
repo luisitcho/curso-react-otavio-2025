0326f097b6c7ce5684116f3f1ffa79adbfa4744d
luisitcho <luishenrisc1@gmail.com>
2025-04-06 21:09:29 -0300

    curso react - Capturando o evento submit do formulário (onSubmit com preventDefault)

--- 1-chronos-pomodoro/src/components/Form/index.tsx
+++ 1-chronos-pomodoro/src/components/Form/index.tsx
@@ -4,8 +4,16 @@ import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 
 export function Form() {
+    function handleCreateNewTask(event: FormEvent<HTMLFormElement>): void {
+        event.preventDefault();
+        console.log(`Foi ${new Date().toLocaleDateString()}`);
+    }
+
     return (
-        <form className='form flex flex-col justify-center gap-4'>
+        <form
+            className='form flex flex-col justify-center gap-4'
+            onSubmit={handleCreateNewTask}
+        >
             <fieldset className='fieldset flex flex-col justify-center items-center gap-4'>
                 <DefaultInput label='task' id='texto' type='text' />
             </fieldset>
