ad768ee98704b27305abfc7f22885c2e4457fd4d
luisitcho <luishenrisc1@gmail.com>
2025-07-19 19:49:22 -0300

    curso react - Lógica do status da tarefa | Em progresso, Completa, Interrompida ou Abandonada

---
+++ 1-chronos-pomodoro/src/utils/getTaskStatus.ts
@@ -0,0 +1,9 @@
+import { TaskModel } from '../models/TaskModel';
+
+export function getTaskStatus(task: TaskModel, activeTask: TaskModel | null) {
+    if (task.completeDate) return 'Concluída';
+    if (task.interruptDate) return 'Interrompida';
+    if (task.id === activeTask?.id) return 'Em Progresso';
+
+    return 'Abandonada';
+}
