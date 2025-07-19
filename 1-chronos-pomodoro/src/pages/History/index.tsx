ed83157bff431f3050e631510cb41fbac46aa0ee
luisitcho <luishenrisc1@gmail.com>
2025-07-19 20:42:35 -0300

    curso react - Ordenando o array de tarefas na interface + função sortTasks

--- 1-chronos-pomodoro/src/pages/History/index.tsx
+++ 1-chronos-pomodoro/src/pages/History/index.tsx
@@ -8,9 +8,11 @@ import styles from './styles.module.scss';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { formatDate } from '../../utils/formatDate';
 import { getTaskStatus } from '../../utils/getTaskStatus';
+import { sortTasks } from '../../utils/sortTasks';
 
 export function History() {
     const { state } = useTaskContext();
+    const sortedTasks = sortTasks({ tasks: state.tasks });
 
     return (
         <MainTemplate>
@@ -42,7 +44,7 @@ export function History() {
                         </thead>
 
                         <tbody>
-                            {state.tasks.map(task => {
+                            {sortedTasks.map(task => {
                                 const taskTypeDictionary = {
                                     workTime: 'Foco',
                                     shortBreakTime: 'Descanso Curto',
