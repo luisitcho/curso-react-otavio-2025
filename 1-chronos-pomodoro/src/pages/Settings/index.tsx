da494a14f80c718b2f2ebbbc80f8562ce8a9da18
luisitcho <luishenrisc1@gmail.com>
2025-07-20 13:39:09 -0300

    curso react - Capturando o onSubmit (envio) do formulário com ref e useRef dos inputs

--- 1-chronos-pomodoro/src/pages/Settings/index.tsx
+++ 1-chronos-pomodoro/src/pages/Settings/index.tsx
@@ -4,8 +4,28 @@ import { DefaultButton } from '../../components/DefaultButton';
 import { DefaultInput } from '../../components/DefaultInput';
 import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
+import { useRef } from 'react';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function Settings() {
+    const { state } = useTaskContext();
+    const workTimeInput = useRef<HTMLInputElement>(null);
+    const shortBreakTimeInput = useRef<HTMLInputElement>(null);
+    const longBreakTimeInput = useRef<HTMLInputElement>(null);
+
+    function handleSaveSettings(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        const workTime = workTimeInput.current?.value;
+        const shortBreakTime = shortBreakTimeInput.current?.value;
+        const longBreakTime = longBreakTimeInput.current?.value;
+
+        console.log('Settings saved:', {
+            workTime,
+            shortBreakTime,
+            longBreakTime,
+        });
+    }
     return (
         <MainTemplate>
             <Container>
@@ -20,23 +40,41 @@ export function Settings() {
             </Container>
 
             <Container>
-                <form className='form flex flex-col justify-center gap-4'>
+                <form
+                    onSubmit={handleSaveSettings}
+                    className='form flex flex-col justify-center gap-4'
+                >
                     <fieldset className='fieldset flex flex-col justify-center items-center gap-4'>
-                        <DefaultInput id='wordTIme' label='Foco' />
+                        <DefaultInput
+                            id='wordTIme'
+                            label='Foco'
+                            ref={workTimeInput}
+                            defaultValue={state.config.workTime}
+                        />
                     </fieldset>
 
                     <fieldset className='fieldset flex flex-col justify-center items-center gap-4'>
-                        <DefaultInput id='shortBreakTime' label='Descanso curto' />
+                        <DefaultInput
+                            id='shortBreakTime'
+                            label='Descanso curto'
+                            ref={shortBreakTimeInput}
+                            defaultValue={state.config.shortBreakTime}
+                        />
                     </fieldset>
 
                     <fieldset className='fieldset flex flex-col justify-center items-center gap-4'>
-                        <DefaultInput id='longBreakTime' label='Descanso longo' />
+                        <DefaultInput
+                            id='longBreakTime'
+                            label='Descanso longo'
+                            ref={longBreakTimeInput}
+                            defaultValue={state.config.longBreakTime}
+                        />
                     </fieldset>
 
                     <fieldset className='fieldset flex flex-col justify-center items-center gap-4'>
                         <DefaultButton
                             icon={<SaveIcon />}
-                            type='button'
+                            type='submit'
                             aria-label='Salvar configurações'
                             title='Salvar configurações'
                         ></DefaultButton>
