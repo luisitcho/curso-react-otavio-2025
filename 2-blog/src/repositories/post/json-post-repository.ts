81bc0e40c39d106eb0d7b4f6c2aa5684eff55bb0
luisitcho <luishenrisc1@gmail.com>
2025-07-26 16:28:48 -0300

    curso react | seção next.js - Padrão de projeto Repository para Posts - Parte 2

--- 2-blog/src/repositories/post/json-post-repository.ts
+++ 2-blog/src/repositories/post/json-post-repository.ts
@@ -1,12 +1,33 @@
 import { PostModel } from "@/models/post/post-model";
 import { PostRepository } from "./post-respository";
+import { readFile } from "fs/promises";
+import { resolve } from "path";
 
 const ROOT_DIR = process.cwd();
-console.log("Root Directory:", ROOT_DIR);
-
+const JSON_POSTS_PATH = resolve(ROOT_DIR, "src", "db", "seed", "posts.json");
 export class JsonPostRepository implements PostRepository {
-    private async readFromDisk() {}
-    async findAll(): Promise<PostModel[]> {}
+    private async readFromDisk() {
+        const json = await readFile(JSON_POSTS_PATH, "utf-8");
+        const jsonParsed = JSON.parse(json);
+        const { posts } = jsonParsed;
+
+        return posts;
+    }
+
+    async findAll(): Promise<PostModel[]> {
+        const posts = await this.readFromDisk();
+
+        return posts;
+    }
+
+    async findById(id: string): Promise<PostModel> {
+        const posts = await this.findAll();
+        const post = posts.find((post) => post.id === id);
+
+        if (!post) throw new Error("Post not found");
+
+        return post;
+    }
 }
 
-export const postRepository = new JsonPostRepository();
+export const postRepository: PostRepository = new JsonPostRepository();
