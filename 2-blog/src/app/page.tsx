c035812fbce943756ef9c0c730cc3d1ea723af83
luisitcho <luishenrisc1@gmail.com>
2025-07-26 20:44:07 -0300

    curso react | seção next.js - Concluindo o CSS do FeaturedPost com Tailwind CSS

--- 2-blog/src/app/page.tsx
+++ 2-blog/src/app/page.tsx
@@ -20,26 +20,29 @@ export default function Home() {
                                 width={1200}
                                 height={720}
                                 alt="Titulo do post"
-                                className="group-hover:scale-105 transition duration-700"
+                                className="w-full h-full object-cover object-center group-hover:scale-105 transition duration-700"
+                                priority
                             />
                         </Link>
-                        <div>
-                            Lorem, ipsum dolor sit amet consectetur adipisicing
-                            elit. Eaque, nobis. Ad, dignissimos nihil aliquid
-                            dolorum voluptas tenetur. Cumque veritatis libero
-                            ullam repudiandae id aperiam necessitatibus minima
-                            accusamus facere, tempora corporis nihil suscipit
-                            vitae. Voluptatibus, obcaecati molestias libero,
-                            similique quaerat minima neque enim deleniti beatae
-                            culpa distinctio vel repellat vitae qui. Nobis
-                            veritatis qui atque praesentium quam officia
-                            doloribus eius, dicta odio fugit autem ab recusandae
-                            tempore earum a. Quam illo molestias tempore et aut
-                            tenetur perspiciatis dolorem ipsam perferendis
-                            praesentium modi ullam doloremque magni sed suscipit
-                            dolorum eius totam a, cupiditate vitae nostrum
-                            asperiores provident? Cumque illum autem minima!
-                            Deleniti!
+                        <div className="flex flex-col gap-2 xl:justify-center">
+                            <time
+                                dateTime="2025-07-26"
+                                className="text-slate-400 text-sm/tight"
+                            >
+                                26/07/2025 10:00
+                            </time>
+
+                            <h1 className="text-2xl/tight font-extrabold sm:text-4xl">
+                                <Link href="#">Lorem ipsum dolor sit amet consectetur</Link>
+                            </h1>
+
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. In velit dolorem est dolor
+                                porro, doloribus neque, quidem mollitia
+                                doloremque, ad perspiciatis fugiat. Rerum, vel
+                                ex? Impedit ullam harum blanditiis mollitia?
+                            </p>
                         </div>
                     </div>
                 </Container>
