66a8807bf14ba833c4c014461b03ea79b541a926
luisitcho <luishenrisc1@gmail.com>
2025-08-03 20:15:09 -0300

    curso react | seção next.js - Substituindo e modificando elementos HTML no React Markdown

--- 2-blog/src/components/SafeMarkdown/index.tsx
+++ 2-blog/src/components/SafeMarkdown/index.tsx
@@ -22,6 +22,20 @@ export function SafeMarkdown({ markdown, className }: SafeMarkdownProps) {
             <ReactMarkdown
                 rehypePlugins={[rehypeSanitize]}
                 remarkPlugins={[remarkGfm]}
+                components={{
+                    table: ({ node, ...props }) => {
+                        if (!node?.children) return "";
+
+                        return (
+                            <div className="overflow-x-auto">
+                                <table
+                                    className="w-full min-w-[600px]"
+                                    {...props}
+                                />
+                            </div>
+                        );
+                    },
+                }}
             >
                 {markdown}
             </ReactMarkdown>
