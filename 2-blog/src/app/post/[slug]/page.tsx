351b7ad7b69efc7f485a4b554df40c14864b9952
luisitcho <luishenrisc1@gmail.com>
2025-08-02 22:18:45 -0300

    curso react | seção next.js - generateMetadata | Gerando title, description ou outras metas dinâmicamente

--- 2-blog/src/app/post/[slug]/page.tsx
+++ 2-blog/src/app/post/[slug]/page.tsx
@@ -1,10 +1,22 @@
 import { Container } from "@/components/Container";
 import { findPostBySlugCached } from "@/lib/post/queries";
-import { notFound } from "next/navigation";
+import { Metadata } from "next";
 
 type PostSlugPageProps = {
     params: Promise<{ slug: string }>;
 };
+
+export async function generateMetadata({
+    params,
+}: PostSlugPageProps): Promise<Metadata> {
+    const { slug } = await params;
+    const post = await findPostBySlugCached(slug);
+
+    return {
+        title: post.title,
+        description: post.excerpt,
+    };
+}
 
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
     const { slug } = await params;
