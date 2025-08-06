98ced42536dd04b9310a48463a13fbdbdb80c9b4
luisitcho <luishenrisc1@gmail.com>
2025-08-05 21:39:58 -0300

    curso react | seção next.js - Criando o repositório DrizzlePostRepository

--- 2-blog/src/repositories/post/drizzle-post-repository.ts
+++ 2-blog/src/repositories/post/drizzle-post-repository.ts
@@ -1,6 +1,27 @@
 import { PostModel } from "@/models/post/post-model";
 import { PostRepository } from "./post-respository";
-import { readFile } from "fs/promises";
-import { resolve } from "path";
+import { drizzleDb } from "@/db/drizzle";
 
-export class JsonPostRepository implements PostRepository {}
+export class DrizzlePostRepository implements PostRepository {
+    async findAllPublic(): Promise<PostModel[]> {
+        const posts = await drizzleDb.query.posts.findMany({
+            orderBy: (posts, { desc }) => desc(posts.createdAt),
+            where: (posts, { eq }) => eq(posts.published, true),
+        });
+
+        return posts;
+    }
+
+    async findBySlugPublic(slug: string): Promise<PostModel> {}
+
+    async findAll(): Promise<PostModel[]> {}
+
+    async findById(id: string): Promise<PostModel> {}
+}
+
+(async () => {
+    const repo = new DrizzlePostRepository();
+    const posts = await repo.findAllPublic();
+
+    posts.forEach((post) => console.log(post.slug, post.published));
+})();
