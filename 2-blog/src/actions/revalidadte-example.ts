2c093057966273efe3a35ebffcf35970071a4c27
luisitcho <luishenrisc1@gmail.com>
2026-02-04 23:57:21 -0300

    curso react | seção next.js - Data cache e fetching com a Fetch API + Tags e revalidateTag

--- 2-blog/src/actions/revalidadte-example.ts
+++ 2-blog/src/actions/revalidadte-example.ts
@@ -1,10 +1,11 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
+import { revalidateTag } from "next/cache";
 
 export async function revalidateExampleAction(formData: FormData) {
-    const path = formData.get('path') as string;
+    const path = formData.get("path") as string;
     console.log("validate example action called for path:", path);
 
-    revalidatePath(path);
+    //revalidatePath(path);
+    revalidateTag("random-user", "default");
 }
--- 2-blog/src/app/exemplo/[id]/page.tsx
+++ 2-blog/src/app/exemplo/[id]/page.tsx
@@ -1,17 +1,23 @@
 import { revalidateExampleAction } from '@/actions/revalidadte-example';
 import { formatHour } from '@/utils/format-datetime';
 
-export const dynamic = 'force-static';
-export const revalidate = 10;
+//export const dynamic = 'force-static';
+//export const revalidate = 10;
 
 export default async function ExemploPage(params: { params: Promise<{ id: string }> }) {
     const hour = formatHour(Date.now());
-
     const { id } = await params.params;
+
+    const response = await fetch('https://randomuser.me/api/?results=1', {
+        next: { tags: ['random-user'], revalidate: 10 }
+    });
+    const data = await response.json();
+    const name = data.results[0].name.first;
 
     return (
         <main className="min-h-[600px] text-4xl font-bold">
-            <div>Hora: {hour}  (ID: {id})</div>
+            <div>
+                Hora: {hour} (ID: {id}) - Nome: {name}</div>
 
             <form action={revalidateExampleAction} className='py-16'>
                 <input type="hidden" name='path' defaultValue={`/exemplo/${id}`} />
