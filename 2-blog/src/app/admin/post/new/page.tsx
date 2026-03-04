17a09f7ef5cb3aa2836ac61adf293920af7c4239
luisitcho <luishenrisc1@gmail.com>
2026-03-03 23:28:32 -0300

    curso react | seção next.js - Sobre a criação dos componentes de Form com Tailwind CSS, clsx e JSX

--- 2-blog/src/app/admin/post/new/page.tsx
+++ 2-blog/src/app/admin/post/new/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/Button";
+
 export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
     return (
         <div className="flex h-screen items-center justify-center">
-            <h1 className="text-2xl font-bold">Admin Post New Page</h1>
+            <Button className="cursor-pointer">Click me</Button>
         </div>
     );
 }
---
+++ 2-blog/src/components/Button/index.tsx
@@ -0,0 +1,8 @@
+type ButtonProps = {
+} & React.ComponentProps<'button'>;
+
+export function Button({ ...props }: ButtonProps) {
+    return (
+        <button {...props} />
+    );
+}
