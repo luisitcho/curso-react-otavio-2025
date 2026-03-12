d5e6eef19b57a316ae2e5ec610c0d898d837c806
luisitcho <luishenrisc1@gmail.com>
2026-03-11 23:37:53 -0300

    curso react | seção next.js - Concluindo o Button e ajustando os botões do Dialog

--- 2-blog/src/components/Button/index.tsx
+++ 2-blog/src/components/Button/index.tsx
@@ -9,21 +9,27 @@ type ButtonProps = {
 export function Button({
     variant = 'default',
     size = 'md',
+    className = '',
     ...props
 }: ButtonProps) {
     const buttonVariants: Record<ButtonVariants, string> = {
-        default: 'bg-blue-600 text-blue-100',
-        ghost: 'bg-slate-200 text-slate-900',
-        danger: 'bg-red-600 text-red-100',
+        default: 'bg-blue-600 hover:bg-blue-700 text-blue-100',
+        ghost: 'bg-slate-300 hover:bg-slate-400 text-slate-950',
+        danger: 'bg-red-600 hover:bg-red-700 text-red-100',
     };
 
     const buttonSizes: Record<ButtonSizes, string> = {
-        sm: 'px-3 py-1 text-sm',
-        md: 'px-4 py-2 text-base',
-        lg: 'px-6 py-3 text-lg',
+        sm: 'text-xs/tight py-1 px-2 rounded-sm [&_svg]:w-3 [&_svg]:h-3 gap-1',
+        md: 'text-base/tight py-2 px-4 rounded-md [&_svg]:w-4 [&_svg]:h-4 gap-2',
+        lg: 'text-lg/tight py-4 px-6 rounded-lg [&_svg]:w-5 [&_svg]:h-5 gap-3',
     };
 
-    const buttonClasses = `${buttonVariants[variant]} ${buttonSizes[size]}`;
+    const baseClasses =
+        'flex items-center justify-center cursor-pointer transition disabled:bg-slate-200 disabled:text-slate-400 disabled:cursor-not-allowed';
 
-    return <button className={buttonClasses} {...props} />;
+    const buttonClasses = `${buttonVariants[variant]} ${buttonSizes[size]} ${baseClasses} ${className}`
+        .replace(/\s+/g, ' ')
+        .trim();
+
+    return <button {...props} className={buttonClasses} />;
 }
