3ff39fa9144915ad903ad9d67aa44cf5b9973b49
luisitcho <luishenrisc1@gmail.com>
2026-03-12 00:46:23 -0300

    curso react | seção next.js - Formulário de criação e edição do post (ManagePostForm)

--- 2-blog/src/app/admin/post/new/page.tsx
+++ 2-blog/src/app/admin/post/new/page.tsx
@@ -1,52 +1,15 @@
-import { Button } from "@/components/Button";
-import { InputCheckbox } from "@/components/InputCheckbox";
-import { InputText } from "@/components/InputText";
-import { BanIcon, BugIcon, CheckIcon } from 'lucide-react';
+import { ManagePostForm } from "@/components/Admin/ManagePostForm";
+
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
     return (
         <div className="container">
-            <form action='' className='mb-16'>
-                <div className='flex flex-col gap-6'>
-                    <InputText
-                        labelText='Nome'
-                        placeholder='Digite seu nome'
-                        type='password'
-                    />
-                    <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
-
-                    <InputCheckbox labelText='Sobrenome' />
-
-                    <InputText
-                        disabled
-                        labelText='Sobrenome'
-                        placeholder='Digite seu sobrenome'
-                        defaultValue='Olá mundo'
-                    />
-                    <InputText
-                        disabled
-                        labelText='Sobrenome'
-                        placeholder='Digite seu sobrenome'
-                    />
-                    <InputText
-                        labelText='Sobrenome'
-                        placeholder='Digite seu sobrenome'
-                        readOnly
-                    />
-                    <InputText
-                        labelText='Sobrenome'
-                        placeholder='Digite seu sobrenome'
-                        defaultValue='Olá mundo'
-                        readOnly
-                    />
-
-                    <div className='mt-4'>
-                        <Button type='submit'>Enviar</Button>
-                    </div>
-                </div>
-            </form>
+            <>
+                <h1>Criar post</h1>
+                <ManagePostForm />
+            </>
         </div>
     );
 }
