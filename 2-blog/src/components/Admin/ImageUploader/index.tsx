e77ad91294e12a3f6a5a3a976bef08c00529fda3
luisitcho <luishenrisc1@gmail.com>
2026-04-22 21:00:17 -0300

    curso react | seção next.js - ImageUploader - input (file) e botão para upload de imagens

---
+++ 2-blog/src/components/Admin/ImageUploader/index.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { Button } from "@/components/Button";
+import { ImageUpIcon } from "lucide-react";
+import { useRef } from "react";
+
+export function ImageUploader() {
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    function handleChooseFile() {
+        if (!fileInputRef.current) return;
+
+        fileInputRef.current.click();
+    }
+    return (
+        <div className="flex flex-col gap-2 py-4">
+            <Button type="button" className="self-start" onClick={handleChooseFile}>
+                <ImageUpIcon />
+                Enviar uma imagem
+            </Button>
+            <input ref={fileInputRef} className="hidden" type="file" name="file" accept="image/*" />
+        </div>
+    );
+}
