23c91dcd658f77d6bad106e9af213831723ff442
luisitcho <luishenrisc1@gmail.com>
2026-05-04 00:08:19 -0300

    curso react | seção nest.js - Injetando o AuthService no AuthController e testando a rota

--- 3-nest-for-next/src/auth/auth.controller.ts
+++ 3-nest-for-next/src/auth/auth.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+
     @Post('login')
     login() {
-        return 'Login';
+        return this.authService.doLogin();
     }
 }
