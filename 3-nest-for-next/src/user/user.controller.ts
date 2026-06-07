c6c6e55c59587581cfd81d0aaef103e956160926
luisitcho <luishenrisc1@gmail.com>
2026-06-07 19:54:39 -0300

    curso react | seção nest.js - Param - Recebendo parâmetros dinâmicos no Controller da request do NestJS

--- 3-nest-for-next/src/user/user.controller.ts
+++ 3-nest-for-next/src/user/user.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    @Get(':id')
+    findOne(@Param('id') id: string): string {
+        return 'This action returns a user with id: ' + id;
+    }
+}
