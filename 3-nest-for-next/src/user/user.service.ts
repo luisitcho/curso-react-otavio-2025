efdfe58aaef252efa66b9f6f074db99f9bb019ae
luisitcho <luishenrisc1@gmail.com>
2026-06-08 00:38:09 -0300

    curso react | seção nest.js - Criando a Entity User e os campos + colunas na tabela da base de dados

--- 3-nest-for-next/src/user/user.service.ts
+++ 3-nest-for-next/src/user/user.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectRepository(User)
+    private readonly userRepository: Repository<User>) { }
+}
