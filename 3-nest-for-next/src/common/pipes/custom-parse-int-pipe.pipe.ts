01998d40e993b2a51cefef8e8ce05f4d80331469
luisitcho <luishenrisc1@gmail.com>
2026-06-07 23:03:00 -0300

    curso react | seção nest.js - Transformando e validando parâmetros com Pipes + Customizando Pipes

---
+++ 3-nest-for-next/src/common/pipes/custom-parse-int-pipe.pipe.ts
@@ -0,0 +1,9 @@
+import { BadRequestException, ParseIntPipe } from "@nestjs/common";
+
+export class CustomParseIntPipe extends ParseIntPipe {
+    constructor() {
+        super({
+            exceptionFactory: (error) => new BadRequestException(`O parâmetro deve ser um número inteiro válido!`),
+        })
+    }
+}
--- 3-nest-for-next/src/user/user.controller.ts
+++ 3-nest-for-next/src/user/user.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
 
 @Controller('user')
 export class UserController {
     @Get(':id')
-    findOne(@Param('id') id: string): string {
+    findOne(@Param('id', CustomParseIntPipe) id: number) {
         return 'This action returns a user with id: ' + id;
     }
 }
