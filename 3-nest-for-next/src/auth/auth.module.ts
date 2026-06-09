580e26befb598dca73432fe28cb41c5aa41c0ad1
luisitcho <luishenrisc1@gmail.com>
2026-06-09 01:09:36 -0300

    curso react | seção nest.js - Assinando o JWT Token e autenticando o usuário com @nestjs/jwt

--- 3-nest-for-next/src/auth/auth.module.ts
+++ 3-nest-for-next/src/auth/auth.module.ts
@@ -1,9 +1,33 @@
-import { Module } from '@nestjs/common';
+import { InternalServerErrorException, Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
+import { CommonModule } from 'src/common/common.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-    imports: [],
+    imports: [
+        UserModule,
+        CommonModule,
+        JwtModule.registerAsync({
+            useFactory: () => {
+                const secret = process.env.JWT_SECRET;
+
+                if (!secret) {
+                    throw new InternalServerErrorException(
+                        'JWT_SECRET not found in .env',
+                    );
+                }
+
+                return {
+                    secret,
+                    signOptions: {
+                        expiresIn: '1d' as const,
+                    },
+                };
+            },
+        }),
+    ],
     controllers: [AuthController],
     providers: [AuthService],
 })
