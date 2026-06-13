3dcc5b84e3a73705c0a4005ebb5246d7d070c985
luisitcho <luishenrisc1@gmail.com>
2026-06-13 13:39:52 -0300

    curso react | seção nest.js - Lendo (READ - GET) e apagando (DELETE) o usuário que está logado

--- 3-nest-for-next/src/user/user.controller.ts
+++ 3-nest-for-next/src/user/user.controller.ts
@@ -1,19 +1,17 @@
 import {
     Body,
     Controller,
+    Delete,
     Get,
-    Param,
     Patch,
     Post,
     Req,
     UseGuards,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { Request } from 'express';
 import { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserResponseDto } from 'src/user/dto/user-response.dto';
@@ -28,13 +26,9 @@ export class UserController {
 
     @UseGuards(JwtAuthGuard)
     @Get(':id')
-    findOne(
-        @Req() req: AuthenticatedRequest,
-        @Param('id', CustomParseIntPipe) id: number,
-    ) {
-        console.log(req.user.id);
-        console.log(req.user.email);
-        return `olá do controller do user #${id}`;
+    async findOne(@Req() req: AuthenticatedRequest) {
+        const user = await this.userService.findOneByOrFail({ id: req.user.id });
+        return new UserResponseDto(user);
     }
 
     @Post()
@@ -57,6 +51,13 @@ export class UserController {
         @Body() dto: UpdatePasswordDto,
     ) {
         const user = await this.userService.updatePassword(req.user.id, dto);
+        return new UserResponseDto(user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Delete('me')
+    async remove(@Req() req: AuthenticatedRequest) {
+        const user = await this.userService.remove(req.user.id);
         return new UserResponseDto(user);
     }
 }
