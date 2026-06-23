007d387172a6e3bc8d4bf40c5f8ddb88da2b0fce
luisitcho <luishenrisc1@gmail.com>
2026-06-22 23:25:53 -0300

    curso react | seção testes - Vitest - primeiro teste unitário de uma função (Testes automatizados)

---
+++ 5-tests-nextjs-vitest-playwright/src/core/todo/factories/make-new-todo.spec.ts
@@ -0,0 +1,26 @@
+import { describe } from "vitest";
+import { makeNewTodo } from "./make-new-todo";
+
+describe("makeNewTodo (unit)", () => {
+    test("should return a new valid todo", () => {
+        // AAA -> Arrange, Act, Assert
+        // Arrange -> Criar as coisas que eu preciso
+        const expectedTodo = {
+            id: expect.any(String),
+            description: "any_description",
+            createdAt: expect.any(String),
+        };
+
+        // Act
+        const newTodo = makeNewTodo("any_description");
+
+        // Assert
+        // toBe ===
+        // toEqual toStrictEqual
+        // Checando apenas a description
+        expect(newTodo.description).toBe(expectedTodo.description);
+
+        // Checando o objeto inteiro
+        expect(newTodo).toStrictEqual(expectedTodo);
+    });
+});
